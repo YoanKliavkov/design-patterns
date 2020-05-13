a6e0c82a5ab83191a01ba4d63304db02d476743f
YoanKliavkov <60933007+YoanKliavkov@users.noreply.github.com>
2020-05-13 11:02:32 +0300

    Add files via upload

---
+++ Observer/src/Test.java
@@ -0,0 +1,14 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+        System.out.println("Enter Text: ");
+        EventSource eventSource = new EventSource();
+        
+        eventSource.addObserver(event -> {
+            System.out.println("Received response: " + event);
+        });
+
+        eventSource.scanSystemIn();
+    }
+}
